1ef882049e4b19ec15ae30573317d878e4b3f3ca
Seier <seier.bernal@gmail.com>
2024-09-30 20:54:43 -0600

    Se agrega paginación con react-pagination

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
-
+import ReactPaginate from 'react-paginate';
 
 import Navbar from '../../components/Navbar'
 import Eventos from '../../components/Eventos'
 import useEventsData from '../../hooks/useEventsData';
+import styles from './Home.module.css';
 
 const Home = () => {
-    const { events, isLoading, error, fetchEvents } = useEventsData();
+    const { events, isLoading, error, fetchEvents, page } = useEventsData();
     const [searchTerm, setSearchTerm] = useState('');
     const containerRef = useRef();
 
@@ -19,6 +20,10 @@ const Home = () => {
         fetchEvents(`&keyword=${term}`);
     };
 
+    const handlePageClick = ({ selected }) => {
+        console.log(selected);
+    };
+
     const renderEvents = () => {
         if (isLoading) {
             return <div>Cargando resultados...</div>;
@@ -27,14 +32,30 @@ const Home = () => {
         if (error) {
             return <div> Ha ocurrido un error</div>;
         }
+
+        return (
+            <div>
+                <Eventos searchTerm={searchTerm} events={events} />
+                <ReactPaginate
+                    className={styles.pagination}
+                    nextClassName={styles.next}
+                    breakLabel="..."
+                    nextLabel=" >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={page.totalPages}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
+        )
     }
-        
+
 
     return (
         <>
             <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
             {renderEvents()}
-            <Eventos searchTerm={searchTerm} events={events} />
         </>
 
     )
