94672721c9f2312761862e4db245556da72699a5
Seier <seier.bernal@gmail.com>
2024-10-02 16:57:37 -0600

    Se inicia lógica de página detail

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,10 +1,34 @@
-import { useParams} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
 
 const Detail = () => {
     const { eventId } = useParams();
+    const [eventData, setEventData] = useState({});
+    const [error, setError] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
-    console.log(eventId);
+    useEffect(() => {
+        const fetchEventData = async () => {
+            try {
+                const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=PwhunAxX5rMoWJMkTHGyiQGEkRS1LSwQ`);
+                const data = await response.json();
+
+                setEventData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setEventData({})
+                setError(error);
+                setIsLoading(false);
+            }
+        };
+
+        fetchEventData();
+    }, []);
+
+    console.log(eventData);
     return <div> Detail </div>;
 }
+
+
 export default Detail;
 
